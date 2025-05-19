49d3c76bc5886327cacc0adb572ecc6d400cf511
terapeutaLPG <terapeutaLPG@gmail.com>
2025-05-19 23:34:38 +0200

    zmiana nazwy aplikacji, jak i otwierania n aloginpage

--- app/src/main/java/com/example/android/MyAppNavigation.kt
+++ app/src/main/java/com/example/android/MyAppNavigation.kt
@@ -1,6 +1,7 @@
 package com.example.android
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,27 +13,32 @@ import com.example.android.views.HomePage
 import com.example.android.views.LoginPage
 import com.example.android.views.SignupPage
 import com.example.android.views.EditNotePage
+import com.example.firebaseauth.viewmodel.AuthState
+import androidx.compose.runtime.getValue
 
 @Composable
 fun MyAppNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel) {
     val navController = rememberNavController()
     val notesViewModel = NotesViewModel()
+    val authState by authViewModel.authState.observeAsState()
 
-    NavHost(navController = navController, startDestination = Routes.notes) {
+    val startDestination = when (authState) {
+        is AuthState.Authenticated -> Routes.home
+        else -> Routes.login
+    }
+
+    NavHost(navController = navController, startDestination = startDestination) {
         composable(Routes.login) {
             LoginPage(modifier, navController, authViewModel)
         }
         composable(Routes.signup) {
             SignupPage(modifier, navController, authViewModel)
         }
-        composable(Routes.notes) {
+        composable(Routes.home) {
             HomePage(navController, authViewModel, notesViewModel)
         }
         composable(Routes.edit_note) {
             EditNotePage(navController, notesViewModel, authViewModel)
-        }
-        composable(Routes.home) {
-            HomePage(navController, authViewModel, notesViewModel)
         }
     }
 }
